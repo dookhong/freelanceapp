2e8e80366fe8a6bde04293ed73329625563e0af4
Le Trong Nghia Windows PC <trongnghia1811@gmail.com>
2022-07-19 23:18:08 +0700

    first commit

---
+++ lib/bill.dart
@@ -0,0 +1,12 @@
+class Bill {
+  late final String name;
+  late final double amount;
+  late final double price;
+  late final double total;
+  Bill({
+    required this.name,
+    required this.amount,
+    required this.price,
+    required this.total,
+  });
+}
---
+++ lib/store.dart
@@ -0,0 +1,10 @@
+class Store {
+  late final String store_name;
+  late final String store_address;
+  late final String store_phone_number;
+  Store({
+    required this.store_name,
+    required this.store_address,
+    required this.store_phone_number,
+  });
+}
